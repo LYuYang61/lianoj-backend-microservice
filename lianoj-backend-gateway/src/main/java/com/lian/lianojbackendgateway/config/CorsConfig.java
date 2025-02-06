1452cd927b9b16917fbb894779dd0fdaf7e4de4c
lian <2242727324@qq.com>
2025-02-06 17:49:32 +0800

    跨域，权限校验

---
+++ lianoj-backend-gateway/src/main/java/com/lian/lianojbackendgateway/config/CorsConfig.java
@@ -0,0 +1,32 @@
+package com.lian.lianojbackendgateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+import java.util.Arrays;
+
+/**
+ * @author lian
+ * @title CorsConfig
+ * @date 2025/2/6 17:43
+ * @description 跨域配置
+ */
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.setAllowCredentials(true);
+        // todo 实际改为线上真实域名、本地域名
+        config.setAllowedOriginPatterns(Arrays.asList("*"));
+        config.addAllowedHeader("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
+    }
+}
